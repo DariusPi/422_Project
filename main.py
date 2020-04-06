23f3e92c952c22be1fcc16dbe3efe127f09c73e8
DariusPi <darius.piecaitis@mail.mcgill.ca>
2020-04-06 18:00:06 -0400

    commenting and cleanup

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import edge_generator
 import edge
 
 
+# Method that converts an integer to a binary array
 def bitfield(number):
     return [1 if digit == '1' else 0 for digit in bin(number)[2:]]
 
 
+# Boolean method that determines if a better version of the network was already found
 def worsenetwork(current, goods):
     for elem in goods:
         match = True
@@ -21,6 +23,16 @@ def worsenetwork(current, goods):
     return False
 
 
+# Boolean method that determines if there is a path to all nodes
+def isvalidnetwork(arr, cities):
+    for elem in range(1, len(cities)):
+        vis = list()
+        if not findpath(arr, cities[0], cities[elem], vis):
+            return False
+    return True
+
+
+# Boolean recursive method that determines if there is a path from one node to another in a network
 def findpath(arr, current, target, visited):
     visited.append(current)
     for elem in arr:
@@ -45,6 +57,7 @@ def findpath(arr, current, target, visited):
     return False
 
 
+# Quicksort helper method to find partition point based on reliability of edges
 def partition(arr, low, high):
     i = (low - 1)  # index of smaller element
     pivot = arr[high].getReliability()  # pivot
@@ -59,14 +72,15 @@ def partition(arr, low, high):
     return i + 1
 
 
+# Recursive sorting algorithm
 def quicksort(arr, low, high):
     if low < high:
         pi = partition(arr, low, high)
-
         quicksort(arr, low, pi - 1)
         quicksort(arr, pi + 1, high)
 
 
+# Method to find the most reliable spanning tree of a network
 def minspan(arr, j):
     q = 0
     edges = list()
@@ -82,43 +96,33 @@ def minspan(arr, j):
     return edges
 
 
-def sysreliability(arr, bol):
-    rel = 1
-    if bol == 1:
-        for elem in arr:
-            rel = rel * elem.getReliability()
-    else:
-        rel = 0
-        for index in range(0, 2 ** (len(arr))):
-            ntree = list()
-            # build tree
-            encoded = bitfield(index)
-            while len(encoded) != len(arr):
-                encoded.insert(0, 0)
-            # print(encoded)
-            for tes in range(0, len(arr)):
-                if encoded[tes] == 1:
-                    ntree.append(arr[tes])
-            # print(ntree)
-            path = True
-            for elem in range(1, len(city_list)):
-                vis = list()
-                if not findpath(ntree, city_list[0], city_list[elem], vis):
-                    path = False
-                    break
-            if path:
-                # multiply reliability of edges in ntree and 1 - reliability of edges not in ntree
-                # print("path found")
-                product = 1
-                for ed in arr:
-                    if ed in ntree:
-                        product = product * ed.getReliability()
-                    else:
-                        product = product * (1 - ed.getReliability())
-                rel = rel + product
+# Method to find the reliability of a network by enumeration
+def sysreliability(arr):
+    rel = 0
+    for index in range(0, 2 ** (len(arr))):
+        ntree = list()
+        # build tree
+        encoded = bitfield(index)
+        while len(encoded) != len(arr):
+            encoded.insert(0, 0)
+
+        for tes in range(0, len(arr)):
+            if encoded[tes] == 1:
+                ntree.append(arr[tes])
+        path = isvalidnetwork(ntree, city_list)
+        if path:
+            # multiply reliability of edges in ntree and 1 - reliability of edges not in ntree
+            product = 1
+            for ed in arr:
+                if ed in ntree:
+                    product = product * ed.getReliability()
+                else:
+                    product = product * (1 - ed.getReliability())
+            rel = rel + product
     return rel
 
 
+# Method to find the cost of a network
 def syscost(arr):
     cost = 0
     for elem in arr:
@@ -126,6 +130,8 @@ def syscost(arr):
     return cost
 
 
+# Main program begins here
+# Take input and output error if incorrect format
 try:
     file_path = input("Please set input file path: ")
     reliability_goal = float(input("Please enter reliability goal: "))
@@ -136,43 +142,45 @@ except Exception as e:
 
 if reliability_goal < 0.0 or reliability_goal >= 1.0:
     print("reliability goal must be positive and under 1")
+elif cost_constraint < 0.0:
+    print("cost constraint must be positive")
 else:
     city_list, edge_list = edge_generator.generate(file_path)
+    print("city list: ", end='')
     print(city_list)
+    print("edge list: ", end='')
     print(edge_list)
 
+    # sort edges by reliability
     n = len(edge_list)
     k = len(city_list)
     quicksort(edge_list, 0, n - 1)
-    edge_list.reverse()
-    # print(edge_list)
-    tree = minspan(edge_list, k)
-    print(tree)
-    print(sysreliability(tree, 1))
-    print(syscost(tree))
-    # print(sysreliability(tree, 0))
     max_reliability = 0
     max_network = list()
     max_network_index = 0
-    optimal_reliability = sysreliability(edge_list, 0)
+
+    # Determine max possible reliability of network and max cost
+    optimal_reliability = sysreliability(edge_list)
     optimal_cost = syscost(edge_list)
-    print("maximum reliability: ")
-    print(optimal_reliability)
-    print("maximum cost: ")
-    print(optimal_cost)
     found_list = list()
     if reliability_goal > optimal_reliability:
         print("No network possible to meet reliability constraint: ")
     elif cost_constraint > optimal_cost:
-        print("Optimal network found: ")
+        print("\na) and b) Optimal network found: ")
         print(edge_list)
         print("reliability = ")
         print(optimal_reliability)
-        print(" cost :")
+        print("cost :")
         print(optimal_cost)
     else:
-        edge_list.reverse()
-        first_found = False
+        # Network with all edges meets reliability target
+        print("\na) Network that meets reliability goal: ")
+        print(edge_list)
+        print("reliability = ")
+        print(optimal_reliability)
+        print("cost :")
+        print(optimal_cost)
+        print("\nb) Finding Best Network for Cost Constraint: This process can take a while...")
         for idx in range(2 ** (len(edge_list)), (2 ** (len(city_list)) - 1), -1):
             gh = list()
             encod = bitfield(idx)
@@ -182,26 +190,16 @@ else:
                 for ts in range(0, len(edge_list)):
                     if encod[ts] == 1:
                         gh.append(edge_list[ts])
-                if (not first_found) or syscost(gh) <= cost_constraint:
-                    if not worsenetwork(encod, found_list):
-                        temp_reliability = sysreliability(gh, 0)
-                        if not first_found:
-                            if temp_reliability >= reliability_goal:
-                                print("first network that meets reliability goal found : ")
-                                print(gh)
-                                first_found = True
-                                if syscost(gh) <= cost_constraint:
-                                    max_reliability = temp_reliability
-                                    max_network_index = idx
-                                    found_list.append(encod)
-                        else:
-                            found_list.append(encod)
-                            if temp_reliability >= max_reliability:
-                                max_reliability = temp_reliability
-                                max_network_index = idx
+                if (syscost(gh) <= cost_constraint) and not (worsenetwork(encod, found_list)):
+                    found_list.append(encod)
+                    if isvalidnetwork(gh, city_list):
+                        temp_reliability = sysreliability(gh)
+                        if temp_reliability > max_reliability:
+                            max_reliability = temp_reliability
+                            max_network_index = idx
 
         if max_reliability == 0:
-            print("No suitable network found for cost constraint")
+            print("No network found for cost constraint and reliability target")
         else:
             print("Optimal network found: ")
             encode = bitfield(max_network_index)
@@ -213,5 +211,5 @@ else:
             print(max_network)
             print("reliability = ")
             print(max_reliability)
-            print(" cost :")
+            print("cost :")
             print(syscost(max_network))
